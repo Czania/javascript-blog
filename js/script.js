310077553d70bcf059ebb166ce8ffff3c81219b6
Karolina Podczasi <karolina.podczasi@gmail.com>
2020-04-17 22:24:42 -0400

    Add js cod for displaying the article after clicking

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault()
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
 
@@ -8,21 +10,24 @@ function titleClickHandler(event){
 const activeLinks = document.querySelectorAll('.titles a.active')
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
-  console.log('active class was remove from all title links')
 }
   /* add class 'active' to the clicked link */
+console.log('clickedElement:', clickedElement);
+clickedElement.classList.add('active');
 
   /* remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('.post.active')
 for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active')
-  console.log('active class was remove from all articles')
+activeArticle.classList.remove('active')
 }
   /* get 'href' attribute from the clicked link */
-
+const articleSelector=clickedElement.getAttribute('href');
+console.log(articleSelector);
   /* find the correct article using the selector (value of 'href' attribute) */
-
+const targetArticle = document.querySelector(articleSelector);
+console.log(targetArticle);
   /* add class 'active' to the correct article */
+targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
